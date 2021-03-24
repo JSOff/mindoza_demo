6f7156c11e5dcbe1d2afb21d5f41a5a22b6babe2
Julien Soleilhavoup <julien.soleilhavoup@gmail.com>
2021-03-24 15:30:09 +0100

    Add map updates by Matthieu G

---
+++ GitHub/mindoza_demo/FLOORS.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="FLOORS" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="../../@BIBLIOTHEQUE/ground 2 copie.png" width="480" height="480"/>
+</tileset>
---
+++ GitHub/mindoza_demo/WALLS.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="WALLS" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <image source="../../@BIBLIOTHEQUE/walls2 copie.png" width="512" height="480"/>
+</tileset>
