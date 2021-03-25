ac99bf20809b6fbc815c4ba763b93983c0aabc05
JSOff <julien.soleilhavoup@gmail.com>
2021-03-25 13:11:55 +0000

    Deploying to gh-pages from master @ a85b7f1e526a8a9062823c530833b58caabe2430 🚀

--- Mindoza_objects copie.tsx
+++ Mindoza_objects copie.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Mindoza_objects copie" tilewidth="32" tileheight="32" tilecount="240" columns="16">
- <image source="@BIBLIOTHEQUE/Mindoza_objects copie.png" width="512" height="480"/>
+ <image source="./@BIBLIOTHEQUE/Mindoza_objects copie.png" width="512" height="480"/>
 </tileset>
